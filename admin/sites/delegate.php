dbe539d1e5103ec3d6cdb4379dcf282a8a424ec0
vishwaaniruddh <86979352+vishwaaniruddh@users.noreply.github.com>
2025-11-06 16:04:42 +0530

    added card-body instaed card-header

--- admin/sites/delegate.php
+++ admin/sites/delegate.php
@@ -34,7 +34,7 @@ ob_start();
 
 <div class="flex justify-between items-center mb-6">
     <div>
-        <h1 class="text-2xl font-semibold text-gray-900">Delegate Site</h1>
+        <!-- <h1 class="text-2xl font-semibold text-gray-900">Delegate Site</h1> -->
         <p class="mt-2 text-sm text-gray-700">Assign site to vendor for installation</p>
     </div>
     <div>
